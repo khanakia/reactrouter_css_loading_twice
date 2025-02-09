0b8f47e775ac015eb8601161bb034f4f8661bcce
khanakia <khanakia@gmail.com>
2025-02-09 13:46:27 +0530

    some fix

--- app/routes/_layoutn.tsx
+++ app/routes/_layoutn.tsx
@@ -1,11 +1,8 @@
 import { Outlet } from 'react-router';
-import { NuqsAdapter } from 'nuqs/adapters/react-router/v7';
 export default function Layout() {
   return (
     <div>
-      <NuqsAdapter>
-        <Outlet />
-      </NuqsAdapter>
+      <Outlet />
     </div>
   );
 }
